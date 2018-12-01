eec24fc3e5e74f2c7cafbee7d04f4dda63d53b7e
Cristian Harnut <cristian.harnut@xing.com>
2018-12-01 14:28:57 +0100

    workshop paramas

--- tests/steps/init.js
+++ tests/steps/init.js
@@ -1,17 +1,46 @@
+const _ = require('lodash')
+const { promisify } = require('util')
+const awscred = require('awscred')
+const { REGION, STAGE } = process.env
+const AWS = require('aws-sdk')
+AWS.config.region = REGION
+const SSM = new AWS.SSM()
+
 let initialized = false
 
-process.env.order_events_stream = 'orders-dev-cha'
-process.env.restaurant_notification_topic = 'restaurants-dev-cha'
+const getParameters = async (keys) => {
+    const prefix = `/workshop-cha/${STAGE}/`
+    const req = {
+        Names: keys.map(key => `${prefix}${key}`)
+}
+    const resp = await SSM.getParameters(req).promise()
+    return _.reduce(resp.Parameters, function(obj, param) {
+        obj[param.Name.substr(prefix.length)] = param.Value
+        return obj
+    }, {})
+}
 
 const init = async () => {
     if (initialized) {
         return
     }
 
-    process.env.restaurants_api   = "https://b282j7d7ri.execute-api.eu-central-1.amazonaws.com/dev/restaurants"
-    process.env.TEST_ROOT         = "https://b282j7d7ri.execute-api.eu-central-1.amazonaws.com/dev"
-    process.env.restaurants_table = "restaurants-dev-cha"
-    process.env.AWS_REGION        = "eu-central-1"
+    const params = await getParameters([
+        'table_name',
+        'stream_name',
+        'restaurant_topic_name',
+        'url'
+    ])
+
+    console.log('SSM params loaded')
+
+    process.env.TEST_ROOT                     = params.url
+    process.env.orders_api                    = `${params.url}/orders`
+    process.env.restaurants_api               = `${params.url}/restaurants`
+    process.env.restaurants_table             = params.table_name
+    process.env.AWS_REGION                    = REGION
+    process.env.order_events_stream           = params.stream_name
+    process.env.restaurant_notification_topic = params.restaurant_topic_name
 
     initialized = true
 }
